356723e3b52e8064b592c8e667002d2d8ac102a5
Enkhjin Nasanbayar <enkhjinnasanbayar@Enkhjins-MacBook-Air-2.local>
2022-09-16 20:59:21 +0800

    tailwind-s ymr ch bsn avatar aa oruulj irsen

---
+++ src/components/Avatar.js
@@ -0,0 +1,13 @@
+export const Avatar = ({ img, content }) => {
+  return (
+    <div className="flex">
+      <img
+        className="w-10 h-10 rounded-full"
+        src="https://images.unsplash.com/photo-1663245338192-df1893e91851?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+      />
+      <div className="text-inherit">
+        <h1>hi</h1>
+      </div>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoute from './Route';
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
